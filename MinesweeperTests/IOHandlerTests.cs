7bfd59b935ae7ac2e69baaf71907a9064db48b13
Mona Jena <mona.jena@myob.com>
2020-10-26 18:47:32 +1300

    renamed HelperTests.cs to IOHandlerTests.cs

---
+++ MinesweeperTests/IOHandlerTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Linq;
+using Minesweeper;
+using Xunit;
+using Moq;
+using FileStream = Minesweeper.FileStream;
+
+namespace MinesweeperTests
+{
+    public class HelperTests
+    {
+        [Fact]
+        public void TestIfReadFileCanReadFile()
+        {
+            
+        } 
+        
+    }
+    
+}
