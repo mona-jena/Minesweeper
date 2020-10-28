98f67c399b0daedc2df5c6b3429f013bb0955d60
Mona Jena <mona.jena@myob.com>
2020-10-29 09:39:12 +1300

    split AddNumbers() method

--- Minesweeper/Grid.cs
+++ Minesweeper/Grid.cs
@@ -7,173 +7,110 @@ namespace Minesweeper
 {
     public class Grid
     {
-        public int Rows { get; set; }
-        public int Columns { get; set; }
+        public int YMax { get; set; }
+        public int XMax { get; set; }
 
-        private string[,] GridArray;
+        private string[,] _gridArray;
 
+        // MAKE IEnumerable AN ARRAY!
 
         public void StoreGridSize(IEnumerable<string> fileContent)
-         {
-             string line1 = fileContent.First().Trim();
-             
-             Rows = int.Parse(line1.Substring(0,1));
-             Columns = int.Parse(line1.Substring(1,1));
-             GridArray = new string[Columns, Rows];
-             ConvertToArray(fileContent);
-             AddNumbers();
-         }
-         
-         public void ConvertToArray(IEnumerable<string> lines)
-         {
-             var y = 0;
-             foreach (var line in lines.Skip(1))
-             {
-                 var x = 0;
-                 foreach (var character in line.Trim())
-                 {
-                     if (character.ToString() == "*")
-                     {
-                         GridArray[x, y] = character.ToString();
-                     }
-                     else if (character.ToString() == ".")
-                     {
-                         GridArray[x, y] = "0";
-                     }
-                     x++;
-                 }
-                 y++;
-             }
+        {
+            string line1 = fileContent.First().Trim();
 
-             for (int i = 0; i < Rows; i++)
-             {
-                 for (int j = 0; j < Columns; j++)
-                 {
-                     Debug.WriteLine("[{0},{1}] = {2}", i, j, GridArray[j, i]);
-                 }
-             }
-             
-         }
+            YMax = int.Parse(line1.Substring(0, 1));
+            XMax = int.Parse(line1.Substring(1, 1));
+            _gridArray = new string[YMax, XMax];
+            ConvertToArray(fileContent);
+            AddNumbers();
+        }
 
-         private void AddNumbers()
-         {
-             int xUpperBound = GridArray.GetUpperBound(0);
-             int yUpperBound = GridArray.GetUpperBound(1);
-             for (int x = 0; x <= xUpperBound; x++) {
-                 for (int y = 0; y <= yUpperBound; y++) {
-                     string value = GridArray[x, y];
+        public void ConvertToArray(IEnumerable<string> lines)
+        {
+            var y = 0;
+            while (y < YMax)
+            {
+                foreach (var line in lines.Skip(1))
+                {
+                    var x = 0;
+                    while (x < XMax)
+                    {
+                        var eachLine = line.Trim();
+                        foreach (var character in eachLine)
+                        {
+                            if (character.ToString() == "*")
+                            {
+                                _gridArray[y, x] = character.ToString();
+                            }
+                            else if (character.ToString() == ".")
+                            {
+                                _gridArray[y, x] = "0";
+                            }
 
-                     if (value == "*")
-                     {
-                         if ((x - 1 >= 0) && (y - 1 >= 0))
-                         {
-                             int coordinateScore;
-                             bool success = Int32.TryParse(GridArray[x - 1, y - 1], out coordinateScore);
-                             if (success)
-                             {
-                                 GridArray[x - 1, y - 1] = (coordinateScore + 1).ToString();
-                             }
-                         }
+                            x++;
+                        }
+                    }
 
-                         if (y - 1 >= 0)
-                         {
-                             int coordinateScore;
-                             bool success = Int32.TryParse(GridArray[x, y - 1], out coordinateScore);
-                             if (success)
-                             {
-                                 GridArray[x, y - 1] = (coordinateScore + 1).ToString();
-                             }
-                         }
+                    y++;
+                }
+            }
+        }
 
-                         if ((x + 1 <= xUpperBound) && (y - 1 >= 0))
-                         {
-                             int coordinateScore;
-                             bool success = Int32.TryParse(GridArray[x + 1, y - 1], out coordinateScore);
-                             if (success)
-                             {
-                                 GridArray[x + 1, y - 1] = (coordinateScore + 1).ToString();
-                             }
-                         }
+        private void AddNumbers()
+        {
+            for (int i = 0; i < YMax; i++)
+            {
+                for (int j = 0; j < XMax; j++)
+                {
+                    if (_gridArray[j, i] == "*")
+                    {
+                        CalculateScore(j, i);
+                    }
+                }
+            }
+        }
 
-                         if (x + 1 <= xUpperBound)
-                         {
-                             int coordinateScore;
-                             bool success = Int32.TryParse(GridArray[x + 1, y], out coordinateScore);
-                             if (success)
-                             {
-                                 GridArray[x + 1, y] = (coordinateScore + 1).ToString();
-                             }
-                         }
+        private void CalculateScore(int y, int x)
+        {
+            foreach (var n in GetNeighbours(x, y))
+            {
+                if (int.TryParse(_gridArray[n.x, n.y], out var number))
+                {
+                    _gridArray[n.x, n.y] = (number + 1).ToString();
+                }
+            }
+        }
 
-                         if ((x + 1 <= xUpperBound) && (y + 1 <= yUpperBound))
-                         {
-                             int coordinateScore;
-                             bool success = Int32.TryParse(GridArray[x + 1, y + 1], out coordinateScore);
-                             if (success)
-                             {
-                                 GridArray[x + 1, y + 1] = (coordinateScore + 1).ToString();
-                             }
-                         }
+        private List<(int x, int y)> GetNeighbours(int x, int y)
+        {
+            List<(int x, int y)> neighbours = new List<(int, int)>();
+            var xValues = new List<int> {x - 1, x, x + 1}.Where(x => x >= 0 && x < XMax);
+            var yValues = new List<int> {y - 1, y, y + 1}.Where(y => y >= 0 && y < YMax);
 
-                         if (y + 1 <= yUpperBound)
-                         {
-                             int coordinateScore;
-                             bool success = Int32.TryParse(GridArray[x, y + 1], out coordinateScore);
-                             if (success)
-                             {
-                                 GridArray[x, y + 1] = (coordinateScore + 1).ToString();
-                             }
-                         }
+            foreach (var i in xValues)
+            foreach (var j in yValues)
+                neighbours.Add((i, j));
 
-                         if ((x - 1 >= 0) && (y + 1 <= yUpperBound))
-                         {
-                             int coordinateScore;
-                             bool success = Int32.TryParse(GridArray[x - 1, y + 1], out coordinateScore);
-                             if (success)
-                             {
-                                 GridArray[x - 1, y + 1] = (coordinateScore + 1).ToString();
-                             }
-                         }
+            neighbours.Remove((x, y));
 
-                         if (x - 1 >= 0)
-                         {
-                             int coordinateScore;
-                             bool success = Int32.TryParse(GridArray[x - 1, y], out coordinateScore);
-                             if (success)
-                             {
-                                 GridArray[x - 1, y] = (coordinateScore + 1).ToString();
-                             }
-                         }
-                     }
-                 }
-             }
+            return neighbours;
+        }
 
-             for (int i = 0; i < Rows; i++)
-             {
-                 for (int j = 0; j < Columns; j++)
-                 {
-                     Debug.WriteLine("[{0},{1}] = {2}", j, i, GridArray[j, i]);
-                 }
-             }
-             
-         }
-         
-         
-         public string PrintGrid()
-         {
-             string matrixGrid = "";
-             for (int y = 0; y < GridArray.GetLength(1); y++)
-             {
-                 for (int x = 0; x < GridArray.GetLength(0); x++)
-                 {
-                     matrixGrid += GridArray[x, y];
-                 }
 
-                 matrixGrid += "\n";
-             }
+        public string PrintGrid()
+        {
+            string matrixGrid = "";
+            for (int y = 0; y < _gridArray.GetLength(1); y++)
+            {
+                for (int x = 0; x < _gridArray.GetLength(0); x++)
+                {
+                    matrixGrid += _gridArray[x, y];
+                }
 
-             return matrixGrid;
-         }
+                matrixGrid += "\n";
+            }
 
+            return matrixGrid;
+        }
     }
 }
